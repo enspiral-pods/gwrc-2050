d9128bb5c5135e6f22f4a5d918c5784ac0ae4698
simontegg <simon@enspiral.com>
2015-11-20 14:04:35 +1300

    just use colors with no fancy linear gradient

--- source/javascripts/charts/summary.js
+++ source/javascripts/charts/summary.js
@@ -1,6 +1,18 @@
 define(['knockout', 'd3', 'charts/chart'], function(ko, d3, Chart) {
   'use strict';
 
+  var pink = "#d53980";
+  var peach = "#d43939";
+  var green = "#28a197";
+
+  function color (d) {
+    console.log('d', d)
+    return d >= 80
+      ? green
+      : d < 0
+        ? peach
+        : pink
+  }
 
   function absolute (num) {
     return num >= 0 ? num : -num
@@ -45,27 +57,47 @@ define(['knockout', 'd3', 'charts/chart'], function(ko, d3, Chart) {
     self.xAxis = xAxis;
 
     self.svg.select("#arrow-total-co2-reduction").remove();
-    self.svg.append("linearGradient").data([data])
-        .attr("id", "arrow-gradient")
-        .attr("gradientUnits", "userSpaceOnUse")
-        .attr("x1", x(xMin)).attr("y1", 0)
-        .attr("x2", function(d) { return x(d) }).attr("y2", 0)
-      .selectAll("stop")
-        .data([
-          { offset: "0%", color: "#fff" },
-          { offset: "100%", color: "#fff" }
-        ])
-      .enter().append("stop")
-        .attr("offset", function(d) { return d.offset; })
-        .attr("stop-color", function(d, i) {
-          // New color interpolated between original and white depending on chart %
-          // var colorness = function(color) { return Math.round(color * (data / 100) + 255 * (1 - (data / 100))); }
-          // var color = "rgb("+colorness(90)+","+colorness(99)+","+colorness(120)+")";
-          // return i === 0 ? color : d.color;
-          return data >= 80 ? "#28a197" : "#d53980"
-          }
-        })
-        .attr("stop-opacity", function(d, i) { return i === 0 ? (1 - (data / 100)) : "1"; });
+    // self.svg.append("linearGradient").data([data])
+    //     .attr("id", "arrow-gradient-decrease")
+    //     .attr("gradientUnits", "userSpaceOnUse")
+    //     .attr("x1", x(xMin)).attr("y1", 0)
+    //     .attr("x2", function(d) { return x(d) }).attr("y2", 0)
+    //   .selectAll("stop")
+    //     .data([
+    //       { offset: "0%", color: "#fff" },
+    //       { offset: "100%", color: "#fff" }
+    //     ])
+    //   .enter().append("stop")
+    //     .attr("offset", function(d) { return d.offset; })
+    //     .attr("stop-color", function(d, i) {
+    //       // New color interpolated between original and white depending on chart %
+    //       // var colorness = function(color) { return Math.round(color * (data / 100) + 255 * (1 - (data / 100))); }
+    //       // var color = "rgb("+colorness(90)+","+colorness(99)+","+colorness(120)+")";
+    //       // return i === 0 ? color : d.color;
+    //       return d >= 80 ? "#28a197" : "#d53980"
+    //     })
+    //     .attr("stop-opacity", function(d, i) { return i === 0 ? (1 - (data / 100)) : "1"; });
+    //
+    // self.svg.append("linearGradient").data([data])
+    //     .attr("id", "arrow-gradient-increase")
+    //     .attr("gradientUnits", "userSpaceOnUse")
+    //     .attr("x1", x(xMin)).attr("y1", 0)
+    //     .attr("x2", function(d) { return x(d) }).attr("y2", 0)
+    //   .selectAll("stop")
+    //     .data([
+    //       { offset: "0%", color: "#fff" },
+    //       { offset: "100%", color: "#fff" }
+    //     ])
+    //   .enter().append("stop")
+    //     .attr("offset", function(d) { return d.offset; })
+    //     .attr("stop-color", function(d, i) {
+    //       // New color interpolated between original and white depending on chart %
+    //       // var colorness = function(color) { return Math.round(color * (data / 100) + 255 * (1 - (data / 100))); }
+    //       // var color = "rgb("+colorness(90)+","+colorness(99)+","+colorness(120)+")";
+    //       // return i === 0 ? color : d.color;
+    //       return d >= 80 ? "#28a197" : "#d53980"
+    //     })
+    //     .attr("stop-opacity", function(d, i) { return i === 0 ? (1 - (data / 100)) : "1"; });
 
     var bars = self.svg.selectAll(".bar")
         .data([data])
@@ -76,13 +108,14 @@ define(['knockout', 'd3', 'charts/chart'], function(ko, d3, Chart) {
     var arrowBaseOffset = 15;
     bars.enter().append("polygon")
         .attr("class", "bar")
-        .attr('fill', 'url(#arrow-gradient)')
+        .attr('fill', color)
         .attr("points", function(d) {
           var base = x(d) - arrowBaseOffset;
           return [x(0)+" 0", base+" 0", (x(d))+" "+(self.height/2), base+" "+self.height, x(0)+" "+self.height].join(", ");
         })
 
     bars.transition()
+        .attr('fill', color)
         .attr("points", function(d) {
           var base = x(d) - arrowBaseOffset;
           return [x(0)+" 0", base +" 0", (x(d))+" "+(self.height/2), base +" "+self.height, x(0)+" "+self.height].join(", ");
