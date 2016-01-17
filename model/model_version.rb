ae2cab3c48372c33467e40a5391817c2e01c9a76
simontegg <simontegg@localhost.localdomain>
2016-01-17 19:46:49 +1300

    model compile

--- model/model_version.rb
+++ model/model_version.rb
@@ -1,1 +1,1 @@
-def Model.last_modified_date() @last_modified_date ||= Time.utc(*[27, 2, 22, 3, 11, 2015, 2, 307, true, "NZDT"]); end
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[23, 27, 14, 14, 1, 2016, 4, 14, true, "NZDT"]); end
