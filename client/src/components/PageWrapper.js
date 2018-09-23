a182f547c02e519e67a8b355cd59fd8c9be2881b
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-09-24 11:53:50 +1200

    add PageWrapper, might be useful

---
+++ client/src/components/PageWrapper.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { Box } from 'rebass/emotion'
+
+export default props => <Box p={15} {...props} />
--- client/src/pages/Emissions.js
+++ client/src/pages/Emissions.js
@@ -5,6 +5,7 @@ import pick from 'lodash/pick'
 import values from 'lodash/values'
 import keys from 'lodash/keys'
 
+import PageWrapper from '../components/PageWrapper'
 import Graph from '../components/Graph'
 import MobileLegend from '../components/MobileLegend'
 
@@ -21,14 +22,14 @@ const Emissions = ({ energyEmissions }) => {
   const graphNames = keys(usedData)
 
   return (
-    <Box>
+    <PageWrapper>
       <Graph
         name={'Greenhouse Gas Emissions'}
         axes={'ktCO2/yr / Date'}
         data={graphAreas}
       />
       <MobileLegend data={graphNames} />
-    </Box>
+    </PageWrapper>
   )
 }
 
