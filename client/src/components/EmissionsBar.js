71ede6237d10483332211470ffc7d13edf74f5ab
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-09-26 15:04:05 +1200

    text instead of graphic for emissions increase

--- client/src/components/EmissionsBar.js
+++ client/src/components/EmissionsBar.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Box, Flex } from 'rebass'
+import { Box, Flex, Heading } from 'rebass'
 
 import FlexWithExtras from './FlexWithExtras'
 
 export default ({ emissionsDecrease }) => {
+  if (!emissionsDecrease) {
+    return null
+  }
+  const prefix =
+    Math.sign(emissionsDecrease) === 1 ? 'Decrease of' : 'Increase of'
   return (
-    <Flex p={20}>
-      <svg height={'100%'} width={'100%'}>
+    <Flex justifyContent={'center'} p={20}>
+      <Heading>{`${prefix} ${Math.abs(
+        (emissionsDecrease * 100).toFixed(2)
+      )}%`}</Heading>
+      {/* <svg height={'100%'} width={'100%'}>
         <rect x='0' y='0' width='100%' height='10' fill={'black'} />
         <rect
           x='50%'
@@ -15,7 +23,7 @@ export default ({ emissionsDecrease }) => {
           height='10'
           fill={'red'}
         />
-      </svg>
+      </svg> */}
     </Flex>
   )
 }
