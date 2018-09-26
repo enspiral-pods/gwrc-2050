04455962bd9b8032cff278f4c6768c43c81b9a29
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-09-27 11:50:45 +1200

    update routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,9 +4,9 @@ import Emissions from './pages/Emissions'
 import EnergyDemand from './pages/EnergyDemand'
 
 let routes = {
-  '/': Emissions,
-  '/emissions': Emissions,
-  '/energy-demand': EnergyDemand
+  '/calculator': Emissions,
+  '/calculator/emissions': Emissions,
+  '/calculator/energy-demand': EnergyDemand
 }
 
 export default createRouteBundle(routes)
