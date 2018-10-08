14a4b3ff703768e4efcb87f877f0b7463b117bfc
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-10-09 10:12:52 +1300

    tweaks to the axis styling, custom component

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Flex, Heading, Text } from 'rebass'
-import { VictoryChart, VictoryAxis, VictoryStack, VictoryArea } from 'victory'
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryStack,
+  VictoryArea,
+  VictoryLabel
+} from 'victory'
 
 import LinearGradient from './LinearGradient'
+import GraphLabelMobile from './GraphLabelMobile'
 
 export default ({ name, axes, data }) => {
   if (!data) {
@@ -37,29 +44,25 @@ export default ({ name, axes, data }) => {
         </VictoryStack>
         <VictoryAxis
           tickValues={[0, 1, 2, 3, 4, 5, 6, 7, 8]}
-          tickFormat={[
-            '2010',
-            '2015',
-            '2020',
-            '2025',
-            '2030',
-            '2035',
-            '2040',
-            '2045',
-            '2050'
-          ]}
+          tickFormat={['2010', '', '2020', '', '2030', '', '2040', '', '2050']}
           label={'Date'}
+          axisLabelComponent={<GraphLabelMobile />}
           offsetY={50}
           style={{
             axisLabel: { fill: 'white', fontSize: 20 },
             tickLabels: { fill: 'white', fontSize: 15 },
-            grid: { stroke: 'white', strokeOpacity: '0.1' }
+            grid: {
+              stroke: 'white',
+              strokeOpacity: (d, i) => (i % 2 == 0 ? '0.1' : '0')
+            }
           }}
         />
         <VictoryAxis
           dependentAxis={true}
           tickValues={[-500, 0, 500, 1000, 1500, 2000, 2500]}
+          tickFormat={d => d.toLocaleString()}
           label={'Emissions (ktCO2/yr)'}
+          axisLabelComponent={<GraphLabelMobile />}
           style={{
             axisLabel: { fill: 'white', fontSize: 20 },
             tickLabels: { fill: 'white', fontSize: 15 },
