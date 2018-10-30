24221edd1145239affc9de687e6cbc916328167d
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-10-31 11:29:24 +1300

    WIP towards restyling emissions bar

--- client/src/components/EmissionsBar.js
+++ client/src/components/EmissionsBar.js
@@ -10,20 +10,20 @@ export default ({ emissionsDecrease }) => {
   const prefix =
     Math.sign(emissionsDecrease) === 1 ? 'Decrease of' : 'Increase of'
   return (
-    <Flex justifyContent={'center'} p={20}>
+    <Flex flexDirection={'column'} justifyContent={'center'} p={20}>
       <Heading>{`${prefix} ${Math.abs(
         (emissionsDecrease * 100).toFixed(2)
       )}%`}</Heading>
-      {/* <svg height={'100%'} width={'100%'}>
+      <svg height={'100%'} width={'100%'}>
         <rect x='0' y='0' width='100%' height='10' fill={'black'} />
         <rect
           x='50%'
           y='0'
-          width={emissionsDecrease}
+          width={`${Math.abs((emissionsDecrease * 100).toFixed(2))}%`}
           height='10'
           fill={'red'}
         />
-      </svg> */}
+      </svg>
     </Flex>
   )
 }
