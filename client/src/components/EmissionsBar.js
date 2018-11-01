403906107edf61eeb57e8224976f3488580d0e64
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-11-01 17:00:22 +1300

    tooltip on emissions bar looking nice

--- client/src/components/EmissionsBar.js
+++ client/src/components/EmissionsBar.js
@@ -9,10 +9,15 @@ export default ({ emissionsDecrease }) => {
   }
 
   const isDecrease = Math.sign(emissionsDecrease) === 1
-  const prefix = isDecrease ? 'Decrease of' : 'Increase of'
+  const postfix = isDecrease ? 'Decrease' : 'Increase'
   const roundedChange = (emissionsDecrease * 100).toFixed(2)
   const absoluteChange = Math.abs(roundedChange)
   const percentageChange = absoluteChange / 100
+
+  const tooltipWidth = 150
+  const barXPointAsWidthPercentage = isDecrease
+    ? `${percentageChange * 50 + 50}%`
+    : `${(1 - percentageChange) * 50}%`
 
   return (
     <Flex
@@ -21,16 +26,51 @@ export default ({ emissionsDecrease }) => {
       width={'100%'}
       p={20}
     >
-      <Heading>{`${prefix} ${absoluteChange}%`}</Heading>
       <svg height={'100%'} width={'100%'}>
-        <rect x='0' y='0' width='100%' height='5' fill={'black'} />
-        <rect
-          x={isDecrease ? '50%' : `${(1 - percentageChange) * 50}%`}
-          y='0'
-          width={`${percentageChange * 50}%`}
-          height='5'
-          fill={'red'}
-        />
+        <g>
+          <rect x='0' y='45' width='100%' height='5' fill={'black'} />
+          <rect
+            x={isDecrease ? '50%' : `${(1 - percentageChange) * 50}%`}
+            y='45'
+            width={`${percentageChange * 50}%`}
+            height='5'
+            fill={'red'}
+          />
+        </g>
+        <g>
+          <rect
+            x={barXPointAsWidthPercentage}
+            y='0'
+            rx={3}
+            ry={3}
+            width={tooltipWidth}
+            height='30'
+            fill={'red'}
+            transform={`translate(-${tooltipWidth / 2})`}
+          />
+          <svg
+            overflow={'visible'}
+            x={barXPointAsWidthPercentage}
+            y={30}
+            height={5}
+          >
+            <polygon
+              fill={'red'}
+              points={`0,0 10,0 5,5`}
+              transform={`translate(-5)`}
+            />
+          </svg>
+          <text
+            x={barXPointAsWidthPercentage}
+            y='15'
+            stroke={'white'}
+            fill={'white'}
+            fontSize={'14px'}
+            fontFamily={'Inter-UI-Regular'}
+            alignmentBaseline={'middle'}
+            textAnchor={'middle'}
+          >{`${Math.round(absoluteChange)}% CO2 ${postfix}`}</text>
+        </g>
       </svg>
     </Flex>
   )
