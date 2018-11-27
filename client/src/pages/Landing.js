5f552d7dfbbd6785166d6f8eb932024d8a46b6b9
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-11-27 18:52:22 +1300

    data page added

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -18,46 +18,7 @@ const Landing = props => {
   return (
     <Flex flexDirection={'column'} width={'100%'}>
       <Flex flexDirection={'column'} width={'100%'} px={200}>
-        <Flex
-          flexDirection={'row'}
-          justifyContent={'space-between'}
-          width={'100%'}
-          py={40}
-        >
-          <Flex flexDirection={'row'} alignItems={'center'}>
-            <Image src={'https://via.placeholder.com/48'} />
-            <TextMedium color={'black'} px={20}>
-              Wellington Region 2050 Emissions Calculator
-            </TextMedium>
-          </Flex>
-          <Flex flexDirection={'row'} alignItems={'center'}>
-            <Link href={'/data'} css={{ minWidth: '120px' }}>
-              <TextRegular
-                fontFamily={'bold'}
-                fontSize={16}
-                color={'black'}
-                css={{ textDecoration: 'underline', opacity: 0.6 }}
-              >
-                About the data
-              </TextRegular>
-            </Link>
-            <Link href={'/calculator'}>
-              <Button
-                variant={'landingGray'}
-                ml={40}
-                css={{ height: '50px', minWidth: '100px' }}
-              >
-                <TextRegular
-                  fontFamily={'bold'}
-                  fontSize={16}
-                  color={'background'}
-                >
-                  Launch
-                </TextRegular>
-              </Button>
-            </Link>
-          </Flex>
-        </Flex>
+        <Navbar />
 
         <Flex flexDirection={'row'} py={60}>
           <Flex flexDirection={'column'} width={'50%'}>
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,6 +1,7 @@
 export default {
   breakpoints: ['800px'],
   colors: {
+    lightBackground: '#F3F3F3',
     background: '#262D33',
     darkBackground: '#1A1F23',
     red: '#FF5252',
