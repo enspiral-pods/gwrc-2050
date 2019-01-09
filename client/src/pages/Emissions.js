52b4e7fef7d14d2fec7a26dcea28e3d6c99c4f68
Michael Smith <michael@msmith.nz>
2019-01-09 16:50:56 +1300

    fix axes tick values

--- client/src/pages/Emissions.js
+++ client/src/pages/Emissions.js
@@ -46,22 +46,7 @@ const Emissions = ({
     '#FC5BFF'
   ]
   console.log('graphAreas', graphAreas)
-  const tickValues = [
-    -1500,
-    -1000,
-    -500,
-    0,
-    500,
-    1000,
-    1500,
-    2000,
-    2500,
-    3000,
-    3500,
-    4000,
-    4500,
-    5000
-  ]
+  const tickValues = [-3000, -2000, -1000, 0, 1000, 2000, 3000, 4000, 5000]
 
   return (
     <Fragment>
@@ -78,7 +63,7 @@ const Emissions = ({
         <Graph
           name={'Greenhouse Gas Emissions'}
           axes={'ktCO2/yr / Date'}
-          // axesTickValues={tickValues}
+          axesTickValues={tickValues}
           data={graphAreas}
           labels={graphNames}
           colors={colors}
