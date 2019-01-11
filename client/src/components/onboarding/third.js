225df16239ebe1eae9aae170824410e1abe1c9db
Michael Smith <michael@msmith.nz>
2019-01-11 16:07:38 +1300

    mobile styling for onboarding

--- client/src/components/onboarding/third.js
+++ client/src/components/onboarding/third.js
@@ -7,8 +7,14 @@ import leverBarDemo from '../../assets/images/onboarding-third.svg'
 export default () => {
   return (
     <Flex flexDirection={'column'} alignItems={'center'}>
-      <Image src={leverBarDemo} height={91} width={453} m={20} mb={40} />
-      <TextRegular color={'black'} fontSize={18} textAlign={'center'}>
+      <Image
+        src={leverBarDemo}
+        height={91}
+        width={453}
+        m={['5px', 20]}
+        mb={['5px', 40]}
+      />
+      <TextRegular color={'black'} fontSize={[16, 18]} textAlign={'center'}>
         Choices are made by adjusting ‘levers’. Each lever has different levels
         from 1 (minimal effort) to 4 (exceptional effort). As you make changes,
         they are visualised in real-time.
