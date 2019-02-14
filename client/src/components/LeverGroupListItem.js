707fb98c4d124c88e7aa8cbe2f825e79b087009f
Michael Smith <michael@msmith.nz>
2019-02-15 10:44:34 +1300

    add tooltips for group levers

--- client/src/components/LeverGroupListItem.js
+++ client/src/components/LeverGroupListItem.js
@@ -1,74 +1,88 @@
-import React, { Component } from 'react'
-import { Flex, Heading, Button, Image, Box, Text } from 'rebass'
+import React, { useState } from 'react'
+import { Flex, Image, Box } from 'rebass'
 import TextRegular from '../components/TextRegular'
 import Lever from './Lever'
+import ToolTip from './ToolTip'
 
 import rightArrow from '../assets/images/right-arrow.svg'
 
-class LeverGroupListItem extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      leverValue: 1
-    }
-  }
-  render () {
-    const {
-      group,
-      levers,
-      doToggleLeverGroup,
-      doChangeGroupLeverValue
-    } = this.props
+export default ({
+  group,
+  levers,
+  doToggleLeverGroup,
+  doChangeGroupLeverValue
+}) => {
+  const [hover, setHover] = useState(false)
+  const [leverValue, setLeverValue] = useState(1)
 
-    return (
-      <Flex
-        flexDirection={'column'}
-        width={'100%'}
-        bg={'darkBackground'}
-        variant={'leverGroup'}
-        p={15}
-        mb={15}
-      >
-        <Box bg={'darkBackground'} onClick={() => doToggleLeverGroup(group)}>
-          <Flex
-            flexDirection={'row'}
-            justifyContent={'space-between'}
-            width={'100%'}
-            pb={'10px'}
+  return (
+    <Flex
+      flexDirection={'column'}
+      width={'100%'}
+      bg={'darkBackground'}
+      variant={'leverGroup'}
+      p={15}
+      mb={15}
+    >
+      <ToolTip hover={hover}>
+        <TextRegular fontSize={14} textAlign={'center'}>
+          {levers[0].group.leverDescriptions[leverValue - 1]}
+        </TextRegular>
+        <div
+          style={{
+            marginLeft: '-10px',
+            borderWidth: '15px 15px 0',
+            borderColor: '#51575C transparent',
+            borderStyle: 'solid',
+            width: 0,
+            marginBottom: '-15px'
+          }}
+        />
+      </ToolTip>
+      <Box bg={'darkBackground'} onClick={() => doToggleLeverGroup(group)}>
+        <Flex
+          flexDirection={'row'}
+          justifyContent={'space-between'}
+          width={'100%'}
+          pb={'10px'}
+        >
+          <TextRegular
+            color={'white'}
+            fontSize={16}
+            textAlign={'left'}
+            fontWeight='bold'
           >
-            <TextRegular
-              color={'white'}
-              fontSize={16}
-              textAlign={'left'}
-              fontWeight='bold'
-            >
-              {group}{' '}
-              <span style={{ fontWeight: 'normal' }}>{`(${
-                levers.length
-              } levers)`}</span>
-            </TextRegular>
-            <Image src={rightArrow} height={24} width={24} mr={10} />
-          </Flex>
-          {levers.map(lever => (
-            <Box
-              bg={'rgba(255,255,255,0.2)'}
-              width={lever.value / 4}
-              mb={1}
-              css={{ borderRadius: '4px', height: '4px' }}
-            />
-          ))}
-        </Box>
+            {group}{' '}
+            <span style={{ fontWeight: 'normal' }}>{`(${
+              levers.length
+            } levers)`}</span>
+          </TextRegular>
+          <Image src={rightArrow} height={24} width={24} mr={10} />
+        </Flex>
+        {levers.map(lever => (
+          <Box
+            bg={'rgba(255,255,255,0.2)'}
+            width={lever.value / 4}
+            mb={1}
+            css={{ borderRadius: '4px', height: '4px' }}
+          />
+        ))}
+      </Box>
+      <Box
+        onMouseEnter={() => setHover(true)}
+        onTouchStart={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onTouchEnd={() => setHover(false)}
+      >
         <Lever
-          value={this.state.leverValue}
+          value={leverValue}
           onValueChange={value => {
-            this.setState({ leverValue: value })
+            setLeverValue(value)
             doChangeGroupLeverValue(value)
           }}
           maxLevel={4}
         />
-      </Flex>
-    )
-  }
+      </Box>
+    </Flex>
+  )
 }
-
-export default LeverGroupListItem
