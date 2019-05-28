f9a2896085d568b2d81d9f7992fadb88f23fc443
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2019-05-28 16:50:54 +1000

    fix onboarding modal images

--- client/src/components/onboarding/fourth.js
+++ client/src/components/onboarding/fourth.js
@@ -7,7 +7,7 @@ import fourth from '../../assets/images/onboarding-fourth.svg'
 export default () => {
   return (
     <Flex flexDirection={'column'} alignItems={'center'}>
-      <Image src={fourth} height={147} width={454} m={[10, 20]} />
+      <Image src={fourth} width={'100%'} m={[10, 20]} />
       <TextRegular color={'black'} fontSize={[16, 18]} textAlign={'center'}>
         As you adjust the levers, you can see changes in total emissions for the
         region as well as changes in energy supply & demand. So, are you ready?
--- client/src/components/onboarding/second.js
+++ client/src/components/onboarding/second.js
@@ -7,13 +7,7 @@ import emissionsBarDemo from '../../assets/images/onboarding-second.svg'
 export default () => {
   return (
     <Flex flexDirection={'column'} alignItems={'center'}>
-      <Image
-        src={emissionsBarDemo}
-        height={91}
-        width={453}
-        m={[10, 20]}
-        mb={[10, 40]}
-      />
+      <Image src={emissionsBarDemo} width={'100%'} m={[10, 20]} mb={[10, 40]} />
       <TextRegular color={'black'} fontSize={[16, 18]} textAlign={'center'}>
         This tool allows you to make choices that will impact projected
         emissions for the Wellington Region. By 2050, we need to achieve
